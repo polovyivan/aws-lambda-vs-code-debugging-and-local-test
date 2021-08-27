798f4b86080a52455db49ab3c47cd6f44a5a8172
Ivan Polovyi <ivan.polovyi@sensedia.com>
2021-08-27 17:29:19 -0300

    working version

--- go.mod
+++ go.mod
@@ -2,4 +2,32 @@ module main
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.26.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.26.0 // indirect
+	github.com/aws/aws-sdk-go v1.40.31 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.8.1
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.6.1
+	github.com/aws/smithy-go v1.7.0 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.3.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.2.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.13.0
+	github.com/aws/aws-sdk-go-v2/service/sso v1.3.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.6.2 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.2.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+var (
+	awsRegion   string
+	awsEndpoint string
+	bucketName  string
+
+	s3svc *s3.Client
+)
+
+func init() {
+	awsRegion = os.Getenv("REGION")
+	awsEndpoint = os.Getenv("BUCKET_ENDPOINT")
+	bucketName = os.Getenv("BUCKET_NAME")
+
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if awsEndpoint != "" {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           awsEndpoint,
+				SigningRegion: awsRegion,
+			}, nil
+		}
+
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(awsRegion),
+		config.WithEndpointResolver(customResolver),
+	)
+	if err != nil {
+		log.Fatalf("Cannot load the AWS configs: %s", err)
+	}
+
+	s3svc = s3.NewFromConfig(awsCfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+}
+
+func uploadFileToS3(fileName string, fileContent string) {
+    s3Key := fmt.Sprintf("%s.txt", fileName)   
+    log.Printf("==>bucketName %q ",  bucketName)
+    body := []byte(fileContent)
+	resp, err := s3svc.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:             aws.String(bucketName),
+		Key:                aws.String(s3Key),
+		Body:               bytes.NewReader(body),
+		ContentLength:      int64(len(body)),
+		ContentType:        aws.String("application/text"),
+		ContentDisposition: aws.String("attachment"),
+	})
+
+	log.Printf("S3 PutObject response: %+v", resp)
+
+	if err != nil {
+        log.Printf("Unable to upload %q to %q, %v", fileName, bucketName, err)
+    } else {
+        log.Printf("Successfully uploaded %q to %q\n", fileName, bucketName)
+    }
+}
 
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 
@@ -14,11 +80,12 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
         fmt.Println("Id", message.MessageId)
         fmt.Println("Source", message.EventSource)
         fmt.Println("Body", message.Body)
+        uploadFileToS3(message.MessageId, message.Body);
     }
 
     return nil
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
